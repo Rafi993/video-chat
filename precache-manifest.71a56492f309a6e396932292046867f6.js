959ebd3336e3cac61c7ad817f8f394108eefe4bb
rafi993 <rafi993@protonmail.com>
2020-01-16 20:09:16 +0530

    Initial dist subtree commit

---
+++ precache-manifest.71a56492f309a6e396932292046867f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31954e5567907e572e94ef5b0c67783",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecdd7ab3cdc68732387a",
+    "url": "/static/css/main.c54c697a.chunk.css"
+  },
+  {
+    "revision": "2e5d85fb214ba58a8307",
+    "url": "/static/js/2.cf003862.chunk.js"
+  },
+  {
+    "revision": "ccbc1d8768028cb10e33433b5c30a082",
+    "url": "/static/js/2.cf003862.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ecdd7ab3cdc68732387a",
+    "url": "/static/js/main.fda865ce.chunk.js"
+  },
+  {
+    "revision": "3b82225a8c49728ba715",
+    "url": "/static/js/runtime-main.7c5c2433.js"
+  }
+]);
